9211115e6676d8845cc69ae9a437449d4f2e7999
Guizin141 <guilherme.palomo@escola.pr.gov.br>
2026-05-27 08:32:42 -0300

    .

--- main.js
+++ main.js
@@ -19,17 +19,17 @@ for (let j = 0; j < botoes.length; j++) {
     botoes[j].classList.remove("ativo");
 }
 for ([inicialização]; [condição]; [incremento]) {
-   declaração
-   }
+    declaração
+}
 for (var i = 1; i <= 5; i++) {
-  console.log(i);
+    console.log(i);
 }
 for (var i = 0; i <= 10; i += 2) {
-  console.log(i);
+    console.log(i);
 }
 var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
 for (var i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
+    console.log(frutas[i]);
 }
 let contador = 5;
 contador += 3;
