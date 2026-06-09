c3de86141d71b4f2819824477c131f6d3e5742f8
Guizin141 <guilherme.palomo@escola.pr.gov.br>
2026-06-09 09:36:17 -0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const botoes = document.querySelectorAll(".botao");
+@import url("https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;700&display=swap");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-    };
+:root {
+  --cor-de-fundo: #000000;
+  --rosaclaro: #000000;
+  --branco: #c50f7f00;
+  --botao-ativo: #17752f;
+  --botao-inativo: rgba(58, 55, 94, 0.5);
+  --texto-fundo: rgba(58, 55, 94, 0.3);
 }
