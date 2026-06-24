17aff12141892b1163fb2c77138b72f0e154dd27
Guizin141 <guilherme.palomo@escola.pr.gov.br>
2026-06-24 08:42:17 -0300

    .

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-08");
+const tempoObjetivo2 = new Date("2027-03-02");
+const tempoObjetivo3 = new Date("2026-11-08");
+const tempoObjetivo4 = new Date("2027-03-15");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
