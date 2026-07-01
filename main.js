caf3258f2a494d6d7d4eccba40011255fd7ee0e3
Guizin141 <guilherme.palomo@escola.pr.gov.br>
2026-07-01 08:36:10 -0300

    ..

--- main.js
+++ main.js
@@ -22,9 +22,20 @@ const tempoObjetivo4 = new Date("2027-03-15");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadif(tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  ); else }
+  {}
+  return "Prazo Finalizado";
+dores[2].textContent = calculaTempo(tempoObjetivo3);
 contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo) {
